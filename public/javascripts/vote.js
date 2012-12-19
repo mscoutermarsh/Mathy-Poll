4f652a35841e5a7ba5aa378c7e8c6b1ee14a2726
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2012-12-19 17:19:55 -0500

    clean up javascript a little

--- public/javascripts/vote.js
+++ public/javascripts/vote.js
@@ -1,32 +1,10 @@
-            function updateVotes(){
+            function updateVotes(id){
                 $.ajax({
                      type: "GET",
                      cache: false,
-                     url: "votes/1",
+                     url: "votes/" + id,
                      success: function(data) {
-                        $("#votes1").html(data.votes + " Votes");
-                     },
-                     error: function(data) {
-
-                     }
-                });
-                $.ajax({
-                     type: "GET",
-                     cache: false,
-                     url: "votes/2",
-                     success: function(data) {
-                        $("#votes2").html(data.votes + " Votes");
-                     },
-                     error: function(data) {
-
-                     }
-                });
-                $.ajax({
-                     type: "GET",
-                     cache: false,
-                     url: "votes/3",
-                     success: function(data) {
-                        $("#votes3").html(data.votes + " Votes");
+                        $("#votes"+id).html(data.votes + " Votes");
                      },
                      error: function(data) {
 
@@ -34,12 +12,36 @@
                 });
             }
 
+            function confirmVote(id,voteid) {
+              event.preventDefault();
+              $("#confirm"+id).hide();
+
+              $.ajax({
+                 type: "POST",
+                 url: "vote/confirm/"+voteid+"/" + $("#math"+id).val(),
+                 success: function(data) {
+                    $("#confirm"+id).html(data.message);
+                    $("#confirm"+id).show('fast');
+                    updateVotes();
+                 },
+                 error: function(data) {
+                    $("#confirm"+id).hide();
+                    var obj = jQuery.parseJSON(data.responseText);
+                    $("#message"+id).html(obj.message);
+                    $("#message"+id).show('fast');
+                    setTimeout(function() {$('#message'+id).hide();$('#confirm'+id).show();} , 2100);
+                 }
+               });
+            }
+
             $(document).ready(function() {
 
                 var voteid=0;
 
                 // load vote counts
-                updateVotes();
+                updateVotes(1);
+                updateVotes(2);
+                updateVotes(3);
 
                 $("#confirm1").hide();
                 $("#message1").hide();
@@ -72,29 +74,6 @@
                     }
                 });
 
-                 $('#submitMath1').click(function(event) {
-                  event.preventDefault();
-                  $("#confirm1").hide();
-
-
-                  $.ajax({
-                     type: "POST",
-                     url: "confirm/"+voteid+"/" + $("#math1").val(),
-                     success: function(data) {
-                        $("#confirm1").html(data.message);
-                        $("#confirm1").show('fast');
-                        updateVotes();
-                     },
-                     error: function(data) {
-                        $("#confirm1").hide();
-                        var obj = jQuery.parseJSON(data.responseText);
-                        $("#message1").html(obj.message);
-                        $("#message1").show('fast');
-                        setTimeout(function() {$('#message1').hide();$('#confirm1').show();} , 2100);
-                     }
-                   });
-                });
-
                 $('#contestant2').click(function(event) {
                   event.preventDefault();
                   if(!$('#contestant2').hasClass('disabled')){
@@ -117,26 +96,14 @@
                     }
                 });
 
-                 $('#submitMath2').click(function(event) {
-                  event.preventDefault();
-                  $("#confirm2").hide();
-
-                  $.ajax({
-                     type: "POST",
-                     url: "confirm/"+voteid+"/" + $("#math2").val(),
-                     success: function(data) {
-                        $("#confirm2").html(data.message);
-                        $("#confirm2").show('fast');
-                        updateVotes();
-                     },
-                     error: function(data) {
-                        $("#confirm2").hide();
-                        var obj = jQuery.parseJSON(data.responseText);
-                        $("#message2").html(obj.message);
-                        $("#message2").show('fast');
-                        setTimeout(function() {$('#message2').hide();$('#confirm2').show();} , 2100);
-                     }
-                   });
+                $('#submitMath1').click(function(event) {
+                  confirmVote(1,voteid);
+                });
+                $('#submitMath2').click(function(event) {
+                  confirmVote(2,voteid);
+                });
+                $('#submitMath3').click(function(event) {
+                  confirmVote(3,voteid);
                 });
 
                 $('#contestant3').click(function(event) {
@@ -159,28 +126,6 @@
                          }
                    });
                    }
-                });
-
-                 $('#submitMath3').click(function(event) {
-                  event.preventDefault();
-                  $("#confirm1").hide();
-
-                  $.ajax({
-                     type: "POST",
-                     url: "confirm/"+voteid+"/" + $("#math3").val(),
-                     success: function(data) {
-                        $("#confirm3").html(data.message);
-                        $("#confirm3").show('fast');
-                        updateVotes();
-                     },
-                     error: function(data) {
-                        $("#confirm3").hide();
-                        var obj = jQuery.parseJSON(data.responseText);
-                        $("#message3").html(obj.message);
-                        $("#message3").show('fast');
-                        setTimeout(function() {$('#message3').hide();$('#confirm3').show();} , 2100);
-                     }
-                   });
                 });
 
             }); 
