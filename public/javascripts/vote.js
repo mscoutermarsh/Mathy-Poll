dbc0225c74677d558ac49e68bbd28b158310835c
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2012-12-19 18:02:19 -0500

    clean up javascript

--- public/javascripts/vote.js
+++ public/javascripts/vote.js
@@ -1,131 +1,117 @@
-            function updateVotes(id){
-                $.ajax({
-                     type: "GET",
-                     cache: false,
-                     url: "votes/" + id,
-                     success: function(data) {
-                        $("#votes"+id).html(data.votes + " Votes");
-                     },
-                     error: function(data) {
+$(document).ready(function() {
+  var voteid=0;
+  // get # of votes for contestant
+  function updateVotes(contestant){
+      $.ajax({
+           type: "GET",
+           cache: false,
+           url: "votes/" + contestant,
+           success: function(data) {
+              $("#votes"+contestant).html(data.votes + " Votes");
+           },
+           error: function(data) {
 
-                     }
-                });
-            }
+           }
+      });
+  }
+  // Submit a vote
+  function submitVote(id) {
+    $.ajax({
+           type: "POST",
+           url: "vote/"+id,
+           success: function(data) {
+              $("#question"+id).html(data.question);
+              voteid = data.id;
+              $("#confirm"+id).show();
+           },
+           error: function(data) {
+             $("#confirm"+id).hide();
+              var obj = jQuery.parseJSON(data.responseText);
+              $("#message"+id).html(obj.message);
+              $("#message"+id).show('fast');
+           }
+     });
+  }
 
-            function confirmVote(id,voteid) {
-              event.preventDefault();
-              $("#confirm"+id).hide();
+  // Submit the answer to the math problem.
+  function confirmVote(id) {
+    event.preventDefault();
+    $("#confirm"+id).hide();
 
-              $.ajax({
-                 type: "POST",
-                 url: "vote/confirm/"+voteid+"/" + $("#math"+id).val(),
-                 success: function(data) {
-                    $("#confirm"+id).html(data.message);
-                    $("#confirm"+id).show('fast');
-                    updateVotes();
-                 },
-                 error: function(data) {
-                    $("#confirm"+id).hide();
-                    var obj = jQuery.parseJSON(data.responseText);
-                    $("#message"+id).html(obj.message);
-                    $("#message"+id).show('fast');
-                    setTimeout(function() {$('#message'+id).hide();$('#confirm'+id).show();} , 2100);
-                 }
-               });
-            }
+    $.ajax({
+       type: "POST",
+       url: "vote/confirm/"+voteid+"/" + $("#math"+id).val(),
+       success: function(data) {
+          $("#confirm"+id).html(data.message);
+          $("#confirm"+id).show('fast');
+          updateVotes(id);
+       },
+       error: function(data) {
+          $("#confirm"+id).hide();
+          var obj = jQuery.parseJSON(data.responseText);
+          $("#message"+id).html(obj.message);
+          $("#message"+id).show('fast');
+          setTimeout(function() {$('#message'+id).hide();$('#confirm'+id).show();} , 2100);
+       }
+     });
+  }
 
-            $(document).ready(function() {
+  function updateAll() {
+    updateVotes(1);
+    updateVotes(2);
+    updateVotes(3);
+  }
 
-                var voteid=0;
+  // load vote counts
+  updateAll();
 
-                // load vote counts
-                updateVotes(1);
-                updateVotes(2);
-                updateVotes(3);
+  $("#confirm1").hide();
+  $("#message1").hide();
 
-                $("#confirm1").hide();
-                $("#message1").hide();
+  $("#confirm2").hide();
+  $("#message2").hide();
 
-                $("#confirm2").hide();
-                $("#message2").hide();
+  $("#confirm3").hide();
+  $("#message3").hide();
 
-                $("#confirm3").hide();
-                $("#message3").hide();
-                     
-                $('#contestant1').click(function(event) {
-                  event.preventDefault();
-                  if(!$('#contestant1').hasClass('disabled')){
-                      $("#contestant1").hide();
-                      $("#contestant2").addClass('disabled');
-                      $("#contestant3").addClass('disabled');
+  // Submit vote
+  $('#contestant1').click(function(event) {
+    event.preventDefault();
+    if(!$('#contestant1').hasClass('disabled')){
+        $("#contestant1").hide();
+        $("#contestant2").addClass('disabled');
+        $("#contestant3").addClass('disabled');
+        submitVote(1);
+      }
+  });
 
-                      $.ajax({
-                         type: "POST",
-                         url: "vote/1/",
-                         success: function(data) {
-                            $("#question1").html(data.question);
-                            voteid = data.id;
-                            $("#confirm1").show();
-                         },
-                         error: function(data) {
-                           alert("error");
-                         }
-                   });
-                    }
-                });
+  $('#contestant2').click(function(event) {
+    event.preventDefault();
+    if(!$('#contestant2').hasClass('disabled')){
+        $("#contestant2").hide();
+        $("#contestant1").addClass('disabled');
+        $("#contestant3").addClass('disabled');
+        submitVote(2);
+      }
+  });
 
-                $('#contestant2').click(function(event) {
-                  event.preventDefault();
-                  if(!$('#contestant2').hasClass('disabled')){
-                      $("#contestant2").hide();
-                      $("#contestant1").addClass('disabled');
-                      $("#contestant3").addClass('disabled');
+  $('#contestant3').click(function(event) {
+    event.preventDefault();
+    if(!$('#contestant3').hasClass('disabled')){
+        $("#contestant3").hide();
+        $("#contestant1").addClass('disabled');
+        $("#contestant2").addClass('disabled');
+        submitVote(3);
+    }
+  });
 
-                      $.ajax({
-                         type: "POST",
-                         url: "vote/2/",
-                         success: function(data) {
-                            $("#question2").html(data.question);
-                            voteid = data.id;
-                            $("#confirm2").show();
-                         },
-                         error: function(data) {
-                           alert("error");
-                         }
-                   });
-                    }
-                });
-
-                $('#submitMath1').click(function(event) {
-                  confirmVote(1,voteid);
-                });
-                $('#submitMath2').click(function(event) {
-                  confirmVote(2,voteid);
-                });
-                $('#submitMath3').click(function(event) {
-                  confirmVote(3,voteid);
-                });
-
-                $('#contestant3').click(function(event) {
-                  event.preventDefault();
-                  if(!$('#contestant3').hasClass('disabled')){
-                      $("#contestant3").hide();
-                      $("#contestant1").addClass('disabled');
-                      $("#contestant2").addClass('disabled');
-
-                      $.ajax({
-                         type: "POST",
-                         url: "vote/3/",
-                         success: function(data) {
-                            $("#question3").html(data.question);
-                            voteid = data.id;
-                            $("#confirm3").show();
-                         },
-                         error: function(data) {
-                           alert("error");
-                         }
-                   });
-                   }
-                });
-
-            }); 
+  $('#submitMath1').click(function(event) {
+    confirmVote(1);
+  });
+  $('#submitMath2').click(function(event) {
+    confirmVote(2);
+  });
+  $('#submitMath3').click(function(event) {
+    confirmVote(3);
+  });
+});
